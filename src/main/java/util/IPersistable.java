1f48fd9dbfbaa0967a2974631c42b8f9ea79b7a9
Ryan Merritt <ryanmerritt28@gmail.com>
2022-02-15 14:13:58 -0500

    Initial commit.

---
+++ src/main/java/util/IPersistable.java
@@ -0,0 +1,10 @@
+package util;
+
+public interface IPersistable {
+    
+    void writeToDB (String dbname);
+    void writeToFile();
+    int Persist();
+    static final String dbname = "Team1_ReservationSystem";
+    static final String path = "Data/";
+}
